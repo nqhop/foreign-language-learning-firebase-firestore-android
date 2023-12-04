ba7b3b703ac320048b47e406691fdd9f17676922
Nguyễn Quốc Hợp <hop35889@gmail.com>
2023-12-04 22:46:40 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -56,8 +57,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database:19.2.0")
-    implementation("com.google.firebase:firebase-firestore-ktx:21.6.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation ("com.google.android.gms:play-services-auth:19.2.0")
 
 
 }
